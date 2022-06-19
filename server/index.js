aafd3e5c71fcfcf7644fa139872a5c0a82fbb5d7
Sadik-Yasin-Eftee <sadikyasin492@gmail.com>
2022-06-20 02:00:33 +0600

    Connected to the Database

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-import { Express } from "express";
+import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
@@ -7,4 +7,14 @@ const app = express();
 
 app.use(bodyParser.json({ limit: "30mb" , extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb" , extended: true }));
-app.use(cors());
+app.use(cors()); 
+
+const CONNECTION_URL = 'mongodb+srv://Sadik41:Yasin1660Eftee@cluster0.wkoc5.mongodb.net/?retryWrites=true&w=majority'
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL , { useNewUrlParser: true, useUnifiedTopology: true }) //using those use parts to avoid errors in terminal
+    .then(() => app.listen(PORT, () => console.log('Server running on port: ${PORT}')))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify' , false);
