5c79c9918a1acb8c78b88fee1b0b14d9e6ddc160
Sadik-Yasin-Eftee <sadikyasin492@gmail.com>
2022-06-25 01:51:27 +0600

    Connected to the database

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,36 @@
 import React from "react";
+import {Container , Appbar , Typography , Grow , Grid} from "@material-ui/core";
+
+import memories from "./images/memories.png";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+
+import useStyles from "./styles";
 
 const App = () => {
+    const classes = useStyles();
     return(
         <div>
-            <h1>App</h1>
+            <Container maxWidth = "lg">
+                <Appbar className= {classes.appBar} position = "static" color = "inherit">
+                <Typography className={classes.heading} variant = "h2" align = "center">Memories</Typography>
+                <img className= {classes.image} src={memories} alt="memories" height="60" />
+                </Appbar>
+                <Grow in>
+                    <Container>
+                        <Grid container justify="space-between" allignItems="stretch" spacing={3}>
+                            <Grid items xs= {12} sm= {7}>
+                                <Posts />
+                            </Grid>
+                            <Grid items xs = {12} sm= {4}>
+                                <Form />
+                            </Grid>
+                        </Grid>
+                    </Container>
+                </Grow>
+            </Container>
         </div>
-    )
+    );
 }
 
 export default App;
